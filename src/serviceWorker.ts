b54038fab3166e4046912c83751a12ab27e4a0d3
Jon Kepa Uriarte <jkuriarte@ids-industrial.com>
2026-01-28 10:52:16 +0100

    Fix project structure for Vite and Actions deployment

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,7 +1,7 @@
 export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then(
+      navigator.serviceWorker.register('./sw.js').then(
         (registration) => {
           console.log('SW registered: ', registration);
         },
